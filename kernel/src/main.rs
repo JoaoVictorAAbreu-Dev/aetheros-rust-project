a8dbb1a269e1a27e9fda191c781d44a702700033
Joao Victor <joaovictoralvesabreu0812@gmail.com>
2026-09-20 19:27:20 -0300

    fix(kernel): initialize x86 SIMD state at entry

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,8 +3,27 @@
 
 use core::panic::PanicInfo;
 
+static INITIAL_MXCSR: u32 = 0x1f80;
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    unsafe {
+        core::arch::asm!(
+            "mov {scratch}, cr0",
+            "and {scratch}, -13",
+            "or {scratch}, 2",
+            "mov cr0, {scratch}",
+            "mov {scratch}, cr4",
+            "or {scratch}, 0x600",
+            "mov cr4, {scratch}",
+            "fninit",
+            "ldmxcsr [{mxcsr}]",
+            scratch = out(reg) _,
+            mxcsr = in(reg) &INITIAL_MXCSR,
+            options(nostack),
+        );
+    }
+
     aether_kernel::arch::x86_64::boot::enter()
 }
 
